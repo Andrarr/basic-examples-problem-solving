2976194285b38b1a811ad1fda69f965b8b6f20e0
Andra Rrustemi <andraa.rrustemi@gmail.com>
2022-09-08 16:39:53 +0200

    OOP examples

--- examples_Two_Andra/calculator.js
+++ examples_Two_Andra/calculator.js
@@ -1,25 +1,47 @@
-let calculator = function(firstNr, secondNr){
+// let calculator = function(firstNr, secondNr){
     
-    let shuma = 0;
-    function sum(){
-return firstNr + secondNr;
-    }
-    function difference(){
-        return firstNr - secondNr;
-    }        
-    function product(){
-        return firstNr * secondNr;
-    }
-    function divident(){
-        return firstNr * secondNr;
-    }
-    return {sum, difference, product, divident}
+  
+//     function sum(){
+// return firstNr + secondNr;
+//     }
+//     function difference(){
+//         return firstNr - secondNr;
+//     }        
+//     function product(){
+//         return firstNr * secondNr;
+//     }
+//     function divident(){
+//         return firstNr * secondNr;
+//     }
+//     return {sum, difference, product, divident}
     
+// }
+// let twoAndFour = calculator(2, 4);
+// console.log(twoAndFour.sum())
+
+class calculator{
+    constructor(firstNr, secondNr){
+        this.firstNr = firstNr;
+        this.secondNr = secondNr;
+    }
 }
-let twoAndFour = calculator(2, 4);
-console.log(twoAndFour.sum())
+class sum extends calculator {
+    constructor(firstNr, secondNr, nrthree){
+        super(firstNr, secondNr);
+        
+        this.nrthree = nrthree
+    }
 
-
+     sum(){
+         return this.firstNr + this.secondNr
+     }
+ 
+}
+const calculating = new sum(5,2)
+console.log(calculating.sum())
+// class divide extends calculator {
+//     constructor(firstNr, secondNr)
+// }
 
 
 // const calc12And5 = Calculator(12, 5);
